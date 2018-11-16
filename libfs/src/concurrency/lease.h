f13309c34b38ae30ec3622c06f0b20d03a90c752
JianweiChen <jianweichen@utexas.edu>
2018-11-16 15:27:44 -0600

    Finally add lease header file

---
+++ libfs/src/concurrency/lease.h
@@ -0,0 +1,5 @@
+#include "global/types.h"
+mlfs_time_t acquire_read_lease(uint32_t inum);
+mlfs_time_t acquire_write_lease(uint32_t inum);
+void release_read_lease(uint32_t inum);
+void release_write_lease(uint32_t inum);
