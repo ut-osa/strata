ecb7a91ac8629815b218ef323415b2348b0cdfba
JianweiChen <jianweichen@utexas.edu>
2018-11-22 16:35:26 -0600

    update lease API

--- libfs/src/concurrency/lease.h
+++ libfs/src/concurrency/lease.h
@@ -3,13 +3,20 @@
 
 #include "global/types.h"
 // lease time in microseconds
-#define MLFS_LEASE_SEC 0
-#define MLFS_LEASE_USEC 999999
+#define MLFS_LEASE_SEC 10
+#define MLFS_LEASE_USEC 0
+enum lease_action { acquire, release };
+enum file_operation { mlfs_read, mlfs_write, mlfs_create, mlfs_delete };
+typedef char inode_t;
 
-mlfs_time_t acquire_read_lease(uint32_t inum);
-mlfs_time_t acquire_write_lease(uint32_t inum);
-void release_read_lease(uint32_t inum);
-void release_write_lease(uint32_t inum);
+struct mlfs_lease_call {
+    lease_action action;
+    uint32_t inum;
+    file_operation operation;
+    inode_t type;
+};
 
+mlfs_time_t mlfs_acquire_lease(uint32_t inum, file_operation operation, inode_t type);
+void mlfs_release_lease(uint32_t inum);
 
 #endif
