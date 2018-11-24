2b4f9acdd3d35b8b878ac0bcc46168b3cd336c4b
JianweiChen <jianweichen@utexas.edu>
2018-11-24 11:19:21 -0600

    Use path instead inode to modify file ops

--- libfs/src/concurrency/lease.h
+++ libfs/src/concurrency/lease.h
@@ -5,18 +5,22 @@
 // lease time in microseconds
 #define MLFS_LEASE_SEC 10
 #define MLFS_LEASE_USEC 0
-enum lease_action { acquire, release };
-enum file_operation { mlfs_read, mlfs_write, mlfs_create, mlfs_delete, null_op};
+enum lease_action { acquire,
+    release };
+enum file_operation { mlfs_read,
+    mlfs_write,
+    mlfs_create,
+    mlfs_delete };
 typedef char inode_t;
 
 struct mlfs_lease_call {
     lease_action action;
-    uint32_t inum;
+    const char* path;
     file_operation operation;
     inode_t type;
 };
 
-mlfs_time_t mlfs_acquire_lease(uint32_t inum, file_operation operation, inode_t type);
-void mlfs_release_lease(uint32_t inum, file_operation operation, inode_t type);
+mlfs_time_t mlfs_acquire_lease(const char* path, file_operation operation, inode_t type);
+void mlfs_release_lease(const char* path, file_operation operation, inode_t type);
 
 #endif
