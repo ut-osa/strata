1ce5d055d4afb4d8aa0fbc3ca6b629e53c5989c1
Alkaid <zgf574564920@gmail.com>
2019-01-04 15:02:08 -0500

    improve performance stats in kernfs
    add signal handler to reset and report stats in kernfs
    add time stats to storage_dax

--- kernfs/kernfs_interface.h
+++ kernfs/kernfs_interface.h
@@ -1,7 +1,9 @@
 #ifndef _KERN_FS_INTERFACE_
 #define _KERN_FS_INTERFACE_
+#define KERNFSPIDPATH "/tmp/kernfs.pid"
 
 void init_fs(void);
-
+void reset_kernfs_stats(void);
+void show_kernfs_stats(void);
 void shutdown_fs(void);
 #endif
