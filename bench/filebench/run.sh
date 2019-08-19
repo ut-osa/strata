348519d067440b8c4c77256a38b930a2f4ed9779
Souvik Banerjee <souvik1997@gmail.com>
2019-08-19 16:35:42 -0500

    Replace glibc shim with syscall_intercept

--- bench/filebench/run.sh
+++ bench/filebench/run.sh
@@ -2,7 +2,7 @@
 
 PATH=$PATH:.
 SRC_ROOT=../../
-export LD_LIBRARY_PATH=$SRC_ROOT/libfs/lib/nvml/src/nondebug/:$SRC_ROOT/libfs/build:$SRC_ROOT/shim/glibc-build/rt/
+export LD_LIBRARY_PATH=$SRC_ROOT/libfs/lib/nvml/src/nondebug/:$SRC_ROOT/libfs/build
 #LD_PRELOAD=$SRC_ROOT/shim/libshim/libshim.so MLFS=1 MLFS_DEBUG=1 $@
 #LD_PRELOAD=$SRC_ROOT/shim/libshim/libshim.so MLFS=1 MLFS_PROFILE=1 taskset -c 0,7 $@
 LD_PRELOAD=$SRC_ROOT/shim/libshim/libshim.so MLFS=1 MLFS_PROFILE=1 taskset -c 0,7 $@
--- libfs/tests/run.sh
+++ libfs/tests/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 PATH=$PATH:.
-export LD_LIBRARY_PATH=../lib/nvml/src/nondebug/:../build:../../shim/glibc-build/rt/:../lib/libspdk/libspdk/
+export LD_LIBRARY_PATH=../lib/nvml/src/nondebug/:../build:../lib/libspdk/libspdk/
 
-LD_PRELOAD=../../shim/libshim/libshim.so:../lib/jemalloc-4.5.0/lib/libjemalloc.so.2 ${@}
+LD_PRELOAD=../lib/jemalloc-4.5.0/lib/libjemalloc.so.2 ${@}
 #LD_PRELOAD=../../shim/libshim/libshim.so:../lib/jemalloc-4.5.0/lib/libjemalloc.so.2 MLFS_PROFILE=1 ${@}
